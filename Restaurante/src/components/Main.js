f2f72105eb8c873789fb48b9cc7536bac3b14dca
JoabTorress <joabtorres28@gmail.com>
2023-01-19 17:25:55 -0300

    first commit

---
+++ Restaurante/src/components/Main.js
@@ -0,0 +1,25 @@
+import  './Main.module.css'
+
+function Main(){
+    return(
+        <>
+            
+            <main>
+      <div className="logo">
+        
+      </div>
+      <div className="infos">
+         <ul>
+            <li><i className="fa-solid fa-location-dot"></i><p>Avenida 9 de Julho, n 116, São Paulo - SP</p></li>
+            <li><i className="fa-solid fa-phone"></i><p>11 47814-4660</p></li>
+            <li><i className="fa-solid fa-envelope"></i><p>opensauce@gmail.com</p></li>
+            <li><i className="fa-brands fa-instagram"></i><p>@opensauce_off</p></li>
+         </ul>
+      </div>
+   </main>
+        
+    </>
+    )
+}
+
+export default Main
